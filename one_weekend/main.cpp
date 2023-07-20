9fd3d5f7c27f648284c52c6a6540b39bf8fa1158
UponTheSky <sailing.to.orbis@gmail.com>
2023-07-20 20:37:22 +0900

    one weekend: ~ 6

--- one_weekend/main.cpp
+++ one_weekend/main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 
-#include "vec3.h"
-#include "ray.h"
+#include "sphere.h"
+#include "hittable_list.h"
 #include "color.h"
+#include "rtweekend.h"
 
-color ray_color(const ray& r) {
+color ray_color(const ray& r, const hittable& world) {
+  hit_record rec;
+  if (world.hit(r, 0, infinity, rec)) {
+    return 0.5 * (rec.normal + color(1, 1, 1));
+  }
   vec3 unit_direction = unit_vector(r.direction());
   auto t = 0.5 * (unit_direction.y() + 1.0); // rescale to [0, 1]
   return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
@@ -15,6 +20,11 @@ int main() {
   const double aspect_ratio = 16.0 / 9.0;
   const int image_width = 400;
   const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+  // world
+  hittable_list world;
+  world.add(make_shared<sphere>(point3(0, 0, -1), 0.5));
+  world.add(make_shared<sphere>(point3(0, -100.5, -1), 100));
 
   // camera
   double viewport_height = 2.0;
@@ -36,7 +46,7 @@ int main() {
         double u = double(i) / (image_width - 1);
         double v = double(j) / (image_height - 1);
         ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
-        color pixel_color = ray_color(r);
+        color pixel_color = ray_color(r, world);
         write_color(std::cout, pixel_color);
     }
   }
--- one_weekend/vec3.h
+++ one_weekend/vec3.h
@@ -8,8 +8,8 @@ using std::sqrt;
 
 class vec3 {
   public:
-    vec3() : e{0, 0, 0} {}
-    vec3(double e0, double e1, double e2) : e{e0, e1, e2} {}
+    vec3() : e{0, 0, 0} {};
+    vec3(double e0, double e1, double e2) : e{e0, e1, e2} {};
 
     double x() const { return e[0]; }
     double y() const { return e[1]; }
