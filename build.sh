b727b7e83c592ebb017e8b12dc483a0db7c06eb3
UponTheSky <sailing.to.orbis@gmail.com>
2023-07-31 07:43:10 +0900

    removed .cpp files & relocate implementations into the header files

--- build.sh
+++ build.sh
@@ -1,15 +1,17 @@
 # reference: https://renenyffenegger.ch/notes/development/languages/C-C-plus-plus/GCC/create-libraries/index
 
-# create object files
-g++ -std=c++17 -c ray_tracer/scene.cpp -o bin/static/scene.o
-g++ -std=c++17 -c ray_tracer/utils/json_parser.cpp -o bin/static/json_parser.o
+# # create object files
+# g++ -std=c++17 -c ray_tracer/scene.cpp -o bin/static/scene.o
+# g++ -std=c++17 -c ray_tracer/utils/json_parser.cpp -o bin/static/json_parser.o
 
-# create static library
-ar rcs bin/static/libjson_parser.a bin/static/json_parser.o
-ar rcs bin/static/libscene.a bin/static/scene.o
+# # create static library
+# ar rcs bin/static/libjson_parser.a bin/static/json_parser.o
+# ar rcs bin/static/libscene.a bin/static/scene.o
 
-# link statically
-g++ --std=c++17 -Lbin/static -ljson_parser -lscene main.cpp -o bin/app
+# # link statically
+# g++ --std=c++17 -Lbin/static -ljson_parser -lscene main.cpp -o bin/app
+
+g++ --std=c++17 ray_tracer/scene.cpp ray_tracer/utils/json_parser.cpp main.cpp -o main
 
 # ./main > image.ppm
 
--- ray_tracer/utils/json_parser.h
+++ ray_tracer/utils/json_parser.h
@@ -4,6 +4,10 @@
 #include <string>
 #include <map>
 #include <utility> // for std::pair
+#include <algorithm> // for std::find
+#include <fstream> // for std::ifstream
+#include <cassert> // for assert()
+#include <cctype> // for isdigit()
 
 namespace JsonParser {
   using text_it = std::string::iterator;
@@ -17,18 +21,106 @@ namespace JsonParser {
     std::map<std::string, JsonValue>* json;
   };
 
-  void ReadFile(const std::string& filepath, std::string& output);
+  JsonValue ParseJson(const std::string& filepath) {
+    // 1. read the text data from the given file
+    std::string text;
+    ReadFile(filepath, text);
 
-  JsonValue ParsePrimitive(const std::string& text, text_it start, text_it end);
+    // 2. parse the text with the helper function and return
+    text_it start = text.begin();
+    return ParseJsonHelper(text, start);
+  }
+
+  void ReadFile(const std::string& filepath, std::string& output) {
+    std::ifstream file(filepath);
+    std::string line;
+
+    while (std::getline(file, line)) {
+      output.append(line); // append() copies the argument passed as a reference(&std::string)
+    }
+  }
+
+  JsonValue ParsePrimitive(const std::string& text, text_it start, text_it end) {
+    std::string substr = text.substr(start - text.begin(), end - start);
+    size_t float_point_index = substr.find(".");
+
+    if (float_point_index >= (end - start)) { // integer
+      return {.i = std::stoi(substr)};
+    } else { // float(double)
+      return {.d = std::stod(substr) };
+    }
+  }
 
   std::pair<std::string, JsonValue> RetriveKeyValuePair(
     const std::string& text,
     text_it& it
-  );
+  ) {
+    assert(it != text.end());
 
-  JsonValue ParseJsonHelper(const std::string& text, text_it& it);
+    // ignore white spaces & line breaks
+    while (*it == ' ' || *it == '\n') {
+      it++;
+    }
 
-  JsonValue ParseJson(const std::string& filepath);
+    text_it curr_it;
+    std::string key;
+    JsonValue value;
+    // if hit a double quote for the first time, it is a key
+    if (*it == '\"') {
+      curr_it = ++it;
+      while (*it != '\"') {
+        it++;
+      }
+
+      key = text.substr(curr_it - text.begin(), it - curr_it);
+      assert(*(++it) == ':'); // assert that we are parsing the key string
+      it++;
+    }
+
+    // now we need to have its corresponding value
+    while (*it == ' ' || *it == '\n') {
+      it++;
+    }
+
+    if (*it == '{') {
+      // another json format
+      value = ParseJsonHelper(text, it);
+    } else {
+      // primitive value(double or int)
+      curr_it = it;
+      while (isdigit(*it) || *it == '.') {
+        it++;
+      }
+      value = ParsePrimitive(text, curr_it, it);
+    }
+
+    // after parsing the value, check whether the current iterator points to a comma
+    if (*it == ',') {
+      it++;
+    }
+
+    return std::make_pair(key, value);
+  }
+
+  JsonValue ParseJsonHelper(const std::string& text, text_it& it) {
+    assert(*it == '{'); // must start with the left curly bracket
+    it++;
+
+    std::map<std::string, JsonValue>* json_map = new std::map<std::string, JsonValue>;
+
+    do {
+      const auto [key, value] = RetriveKeyValuePair(text, it);
+      (*json_map)[key] = value;
+
+      while (*it == ' ' || *it == '\n') {
+        it++;
+      }
+    } while (*it != '}');
+
+    it++; // after '}'
+
+    return { .json = json_map };
+  }
 }
 
 #endif
