6aa974befb11428533ae1847b480fc5b45f19f83
UponTheSky <sailing.to.orbis@gmail.com>
2024-08-15 21:44:25 +0900

    one-week: ~2.2

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, Write, BufWriter};
+
+const IMAGE_WIDTH: u32 = 256;
+const IMAGE_HEIGHT: u32 = 256;
+
+fn main() -> std::io::Result<()> {
+    let mut stdout = BufWriter::new(io::stdout().lock());
+
+    stdout.write(b"P3\n")?;    
+    stdout.write((format!("{IMAGE_WIDTH} {IMAGE_HEIGHT}\n255\n")).as_bytes())?;
+
+    for j in 0..IMAGE_HEIGHT {
+        for i in 0..IMAGE_WIDTH {
+            let r = (i as f32) / ((IMAGE_WIDTH - 1) as f32);
+            let g = (j as f32) / ((IMAGE_HEIGHT - 1) as f32);
+            let b: f32 = 0.0;
+
+            let ir: i32 = (255.999 * r) as i32;
+            let ig: i32 = (255.999 * g) as i32;
+            let ib: i32 = (255.999 * b) as i32;
+
+            stdout.write(format!("{ir} {ig} {ib}\n").as_bytes())?;
+        }
+    }
+
+    stdout.flush()?;
+
+    Ok(())
 }
