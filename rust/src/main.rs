cd69abb8d5e386a6a4d5a05f4d469a09c32b37a6
UponTheSky <sailing.to.orbis@gmail.com>
2024-08-23 12:04:00 +0900

    one-week: ~ch4(finished)

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -4,10 +4,11 @@ mod ray;
 mod vec3;
 
 use color::{write_color, Color};
+use ray::Ray;
 use point::Point3;
-use vec3::Vector3;
-use std::io::{self, BufWriter, Write};
 use std::cmp::max;
+use std::io::{self, BufWriter, Write};
+use vec3::Vector3;
 
 // image
 const IMAGE_WIDTH: u32 = 400;
@@ -17,8 +18,12 @@ const ASPECT_RATIO: f64 = 16.0 / 9.0;
 const VIEWPORT_HEIGHT: f64 = 2.0;
 const FOCAL_LENGTH: f64 = 1.0;
 
-fn ray_color() -> Color {
-    Color::new_default()
+fn ray_color(ray: &Ray) -> Color {
+    // we're sure the ray is not a zero vector, so we unwrap()
+    let unit_direction = ray.direction().normalize().unwrap();
+    let a = 0.5 * (unit_direction.y() + 1.0);
+
+    (1.0 - a) * Color::new(1.0, 1.0, 1.0) + a * Color::new(0.5, 0.7, 1.0)
 }
 
 fn main() -> std::io::Result<()> {
@@ -33,10 +38,17 @@ fn main() -> std::io::Result<()> {
     let viewport_width = VIEWPORT_HEIGHT * ((IMAGE_WIDTH as f64) / (image_height as f64));
     let camera_center = Point3::new_default();
     let viewport_u = Vector3::new(viewport_width, 0.0, 0.0);
-    let viewport_v = Vector3::new(VIEWPORT_HEIGHT, 0.0, 0.0);
-    
-    // TODO: start from pixel_delta_u
-    
+    let viewport_v = Vector3::new(0.0, -VIEWPORT_HEIGHT, 0.0);
+
+    let pixel_delta_u = viewport_u * (1.0 / (IMAGE_WIDTH as f64));
+    let pixel_delta_v = viewport_v * (1.0 / (image_height as f64));
+
+    let viewport_upper_left = camera_center
+        - Vector3::new(0.0, 0.0, FOCAL_LENGTH)
+        - (viewport_u * 0.5)
+        - (viewport_v * 0.5);
+
+    let pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v); 
 
     stdout.write(b"P3\n")?;
     stdout.write((format!("{IMAGE_WIDTH} {image_height}\n255\n")).as_bytes())?;
@@ -46,12 +58,12 @@ fn main() -> std::io::Result<()> {
             stderr.write(format!("\rScanlines remaining: {} ", image_height - j).as_bytes())?;
             stderr.flush()?;
 
-            let color = Color::new(
-                (i as f64) / ((IMAGE_WIDTH - 1) as f64),
-                (j as f64) / ((image_height - 1) as f64),
-                0.0,
-            );
-            write_color(&mut stdout, &color)?;
+            let pixel_center = pixel00_loc + ((i as f64) * pixel_delta_u) + ((j as f64) * pixel_delta_v);
+            let ray_direction = pixel_center - camera_center;
+            let ray = Ray::new(&camera_center, &ray_direction);
+
+            let pixel_color = ray_color(&ray);
+            write_color(&mut stdout, &pixel_color)?;
         }
     }
 
